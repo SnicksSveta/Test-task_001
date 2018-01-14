0d14f16123efa5819f6dd15b910adf786c28e9bb
Snicks_s <sveta.snicks777@gmail.com>
2018-01-15 00:09:17 +0200

    Assets config

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,14 @@ module.exports = {
           fallback: "style-loader",
           use: ["css-loader", "sass-loader"]
         })
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
       }
     ]
   },
